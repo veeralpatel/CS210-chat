9f442a63a1080dee764a710f8810ed30b85de1f2
Veeral Patel <veeral@paribus.co>
2017-01-15 14:02:33 -0800

    Revert to older code

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
-// Variable port setting for heroku
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 var port = process.env.PORT || 3000;
-
-var app = require('express')();
-var io = require('socket.io').listen(app);
-
-app.listen(port);
-
-// Heroku setting for long polling - assuming io is the Socket.IO server object
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
-// routing
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-// usernames which are currently connected to the chat
-var usernames = {};
-
-io.sockets.on('connection', function (socket) {
-
-    // when the client emits 'sendchat', this listens and executes
-    socket.on('sendchat', function (data) {
-        // we tell the client to execute 'updatechat' with 2 parameters
-        io.sockets.emit('updatechat', socket.username, data);
-    });
-
-    // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function(username){
-        // we store the username in the socket session for this client
-        socket.username = username;
-        // add the client's username to the global list
-        usernames[username] = username;
-        // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'you have connected');
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-        // update the list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-    });
-
-    // when the user disconnects.. perform this
-    socket.on('disconnect', function(){
-        // remove the username from global usernames list
-        delete usernames[socket.username];
-        // update list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-        // echo globally that this client has left
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-    });
+http.listen(port, function(){
+  console.log('listening on ' + port);
 });
